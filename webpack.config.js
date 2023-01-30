f156f5849d8c69f3f7d74dfe36c1a5328308cb80
Vladimir Orlov <satancrew.true@gmail.com>
2023-01-30 16:50:37 +0300

    init: start work under cv, finish settings for project

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+const { merge } = require('webpack-merge');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+const baseConfig = {
+  entry: {
+    main: path.resolve(__dirname, './src/script.ts'),
+  },
+  mode: 'development',
+  module: {
+    rules: [
+      { test: /\.ts$/i, use: 'ts-loader' },
+      {
+        test: /\.(s*)css$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js'],
+  },
+  output: {
+    filename: 'script.js',
+    path: path.resolve(__dirname, './dist'),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, './src/index.html'),
+      filename: 'index.html',
+    }),
+    new CleanWebpackPlugin(),
+  ],
+};
+
+module.exports = ({ mode }) => {
+  const isProductionMode = mode === 'prod';
+  const envConfig = isProductionMode ? require('./webpack.prod.config') : require('./webpack.dev.config');
+  return merge(baseConfig, envConfig);
+};
---
+++ webpack.dev.config.js
@@ -0,0 +1,15 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  devtool: 'inline-source-map',
+  devServer: {
+    static: {
+      directory: path.join(__dirname, './dist'),
+    },
+    compress: true,
+    port: 8080,
+    hot: true,
+    open: true,
+  },
+};
