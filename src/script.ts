8f9dea39bc1590c06a23cc4341d094d5cefb1efe
Vladimir Orlov <satancrew.true@gmail.com>
2023-02-01 14:26:19 +0300

    feat: adaptive and working under light/dark themes

--- src/script.ts
+++ src/script.ts
@@ -28,22 +28,59 @@ navLink.forEach(el => el.addEventListener('click', linkAction));
 
 // Scroll sections active link //
 
-// const sections = document.querySelectorAll('section[id]');
-// console.log(sections, 'sections');
-// const scrollActive = () => {
-//   const scrollY = window.pageYOffset;
+const sections = document.querySelectorAll('section[id]');
+console.log(sections, 'sections');
+const scrollActive = () => {
+  const scrollY = window.pageYOffset;
 
-//   sections.forEach(el => {
-//     const sectionHeight = el.offsetHeight;
-//     const sectionTop = el.offsetTop - 50;
-//     const sectionId = el.getAttribute('id') as string;
+  sections.forEach(el => {
+    console.log(el, 'el');
+    const sectionHeight = (el as HTMLElement).offsetHeight;
+    const sectionTop = (el as HTMLElement).offsetTop - 50;
+    const sectionId = el.getAttribute('id') as string;
 
-//     if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
-//       (document.querySelector('nav__menu a[href*=' + sectionId + ']') as HTMLElement).classList.add('active-link');
-//     } else {
-//       (document.querySelector('nav__menu a[href*=' + sectionId + ']') as HTMLElement).classList.remove('active-link');
-//     }
-//   })
-// }
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      (document.querySelector('nav__menu a[href*=' + sectionId + ']') as HTMLElement).classList.add('active-link');
+    } else {
+      (document.querySelector('nav__menu a[href*=' + sectionId + ']') as HTMLElement).classList.remove('active-link');
+    }
+  })
+}
 
-// window.addEventListener('scroll', scrollActive);
+window.addEventListener('scroll', scrollActive);
+
+// Show scroll top //
+const scrollTop = () => {
+  const scrollTop = document.getElementById('scroll-top') as HTMLElement;
+  if (window.scrollY >= 560) {
+    scrollTop.classList.add('show-scroll')
+  } else {
+    scrollTop.classList.remove('show-scroll');
+  }
+}
+
+window.addEventListener('scroll', scrollTop);
+
+// Dark / Light themes // 
+const themeButton = document.getElementById('theme-button') as HTMLElement;
+const darkTheme = 'dark-theme';
+const iconTheme = 'bx-sun';
+
+const selectedTheme = localStorage.getItem('selected-theme');
+const selectedIcon = localStorage.getItem('selected-icon');
+
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light';
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'bx-moon' : 'bx-sun';
+
+if (selectedTheme) {
+  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme);
+  themeButton.classList[selectedTheme === 'bx-moon' ? 'add' : 'remove'](iconTheme);
+}
+
+themeButton.addEventListener('click', () => {
+  document.body.classList.toggle(darkTheme);
+  document.body.dataset.theme = "dark-theme";
+  themeButton.classList.toggle(iconTheme);
+  localStorage.setItem('selected-theme', getCurrentTheme());
+  localStorage.setItem('selected-icon', getCurrentIcon());
+})
