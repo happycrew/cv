e031acc8c10d22b9b4aedecae02cd3014143def6
Vladimir Orlov <satancrew.true@gmail.com>
2023-02-01 15:30:19 +0300

    feat: add light/dark theme

--- src/script.ts
+++ src/script.ts
@@ -28,26 +28,26 @@ navLink.forEach(el => el.addEventListener('click', linkAction));
 
 // Scroll sections active link //
 
-const sections = document.querySelectorAll('section[id]');
-console.log(sections, 'sections');
-const scrollActive = () => {
-  const scrollY = window.pageYOffset;
+// const sections = document.querySelectorAll('section[id]');
+// console.log(sections, 'sections');
+// const scrollActive = () => {
+//   const scrollY = window.pageYOffset;
 
-  sections.forEach(el => {
-    console.log(el, 'el');
-    const sectionHeight = (el as HTMLElement).offsetHeight;
-    const sectionTop = (el as HTMLElement).offsetTop - 50;
-    const sectionId = el.getAttribute('id') as string;
+//   sections.forEach(el => {
+//     console.log(el, 'el');
+//     const sectionHeight = (el as HTMLElement).offsetHeight;
+//     const sectionTop = (el as HTMLElement).offsetTop - 50;
+//     const sectionId = el.getAttribute('id') as string;
 
-    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
-      (document.querySelector('nav__menu a[href*=' + sectionId + ']') as HTMLElement).classList.add('active-link');
-    } else {
-      (document.querySelector('nav__menu a[href*=' + sectionId + ']') as HTMLElement).classList.remove('active-link');
-    }
-  })
-}
+//     if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+//       (document.querySelector('nav__menu a[href*=' + sectionId + ']') as HTMLElement).classList.add('active-link');
+//     } else {
+//       (document.querySelector('nav__menu a[href*=' + sectionId + ']') as HTMLElement).classList.remove('active-link');
+//     }
+//   })
+// }
 
-window.addEventListener('scroll', scrollActive);
+// window.addEventListener('scroll', scrollActive);
 
 // Show scroll top //
 const scrollTop = () => {
@@ -75,11 +75,17 @@ const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'bx-moo
 if (selectedTheme) {
   document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme);
   themeButton.classList[selectedTheme === 'bx-moon' ? 'add' : 'remove'](iconTheme);
+  (selectedTheme === 'dark') ? document.documentElement.setAttribute('theme', 'dark') : document.documentElement.setAttribute('theme', 'light');
 }
 
 themeButton.addEventListener('click', () => {
+  if (document.documentElement.getAttribute('theme') == 'dark') {
+    console.log('ye')
+    document.documentElement.setAttribute('theme', 'light');
+  } else {
+    document.documentElement.setAttribute('theme', 'dark');
+  }
   document.body.classList.toggle(darkTheme);
-  document.body.dataset.theme = "dark-theme";
   themeButton.classList.toggle(iconTheme);
   localStorage.setItem('selected-theme', getCurrentTheme());
   localStorage.setItem('selected-icon', getCurrentIcon());
